9f8960d7e50476fa2f167def7d205988f62a01b7
tasnim-jawad <tasnimulh453@gmail.com>
2025-05-10 12:18:07 +0600

    js added for navbar

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+  
+let nav_trigger = document.getElementById('nav_trigger');
+let nav_close = document.getElementById('nav_close');
+
+nav_trigger.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('nav_link').classList.toggle('nav_show');
+})
+
+nav_close.addEventListener('click',function(e){
+    e.preventDefault();
+    document.getElementById('nav_link').classList.toggle('nav_show');
+})
